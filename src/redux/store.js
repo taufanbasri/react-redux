4dafc951c0558b41987208c90a28e2c9c5964e54
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-25 21:24:28 +0700

    Redux devtool extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
