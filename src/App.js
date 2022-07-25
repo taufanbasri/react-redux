33de25e421dae420a85e9496dbd68354ce7a17da
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-25 17:31:40 +0700

    Action, Reducer, Store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import { Provider } from "react-redux";
+import "./App.css";
+import CakeConainer from "./components/CakeConainer";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeConainer />
+      </div>
+    </Provider>
   );
 }
 
