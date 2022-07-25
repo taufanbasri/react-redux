88889fef72823617acda2ff230b8c469436e22a1
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-25 17:50:07 +0700

    Prepare for use react-redux with hooks

---
+++ src/components/HookCakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HookCakeContainer() {
+  return (
+    <div>
+      <h2>Num of cakes - </h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+export default HookCakeContainer;
