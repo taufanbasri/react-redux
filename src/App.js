1171d097c33078c7fd0b6e89810c6fd94f06bee2
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-26 09:26:41 +0700

    Asynchronous get request with redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import { Provider } from "react-redux";
 import "./App.css";
-import CakeConainer from "./components/CakeConainer";
-import HookCakeContainer from "./components/HookCakeContainer";
-import IceCreamContainer from "./components/IceCreamContainer";
-import ItemContainer from "./components/ItemContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
+import UserContainer from "./components/UserContainer";
 import store from "./redux/store";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
-        <ItemContainer />
-
-        <h2>Redux with Hooks</h2>
-        <HookCakeContainer />
-
-        <hr />
-        <h2>Only Redux</h2>
-        <CakeConainer />
-
-        <hr />
-        <NewCakeContainer />
-
-        <hr />
-        <IceCreamContainer />
+        <UserContainer />
       </div>
     </Provider>
   );
--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux";
 
-function UserContainer() {
-  return (
-    <div>UserContainer</div>
-  )
+function UserContainer({ userData, fetchUsersData }) {
+  useEffect(() => {
+    fetchUsersData();
+  }, []);
+
+  return userData.loading ? (
+    <h2>Loading</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>User List</h2>
+      <div>
+        {userData &&
+          userData.users &&
+          userData.users.map((user) => <p key={user.id}>{user.name}</p>)}
+      </div>
+    </div>
+  );
 }
 
-export default UserContainer
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsersData: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
